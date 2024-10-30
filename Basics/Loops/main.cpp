5d06381eb040d158327a179fec2a1cb4ee1af2ad
Vardan Mittal <vardanmittal000@gmail.com>
2024-10-30 18:02:30 +0530

    learning loops functions and pass by value and refrence

---
+++ Basics/Loops/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // learning For loop
+    for(int i=0; i<5; i++){ 
+        // for(initialization; condition; increment/decrement)
+        cout << "Vardan Mittal" << endl;
+    }
+
+    // learning while loop
+    int j = 0;
+    while(j<5){
+        // while(condition){ 
+             // code
+             // increment/decrement ** must be there}
+        cout << "Vardan Mittal" << endl;
+        j++;
+    }
+
+    // learning do while loop must run atleast once
+    int k = 0;
+    do{
+        // do{
+            // code
+            // increment/decrement ** must be there
+        // }while(condition);
+        cout << "Vardan Mittal" << endl;
+        k++;
+    }while(k<5);
+
+    return 0;
+}
